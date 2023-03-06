f1f46326288959a9515ef4e0e38d60c630fac2e2
shikun0625 <18017560625@163.com>
2023-03-06 14:32:02 +0900

    新建项目

---
+++ SKRestaurant_iOS/SKRestaurant_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SKRestaurant_iOS
+//
+//  Created by 施　こん on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
